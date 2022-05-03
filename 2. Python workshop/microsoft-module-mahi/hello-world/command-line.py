68049b4238fa0bb1d5cf6452e7ca559bcd67a8ec
maria <mariagedye@gmail.com>
2022-05-03 23:12:07 +1200

    user input

---
+++ 2. Python workshop/microsoft-module-mahi/hello-world/command-line.py
@@ -0,0 +1,18 @@
+import sys
+
+# print(sys.argv)
+# print(sys.argv[0])
+
+# user input
+
+print("Welcome to the greeter program")
+name = input("O ai lou igoa? ")
+print("malo le soifua " + name)
+
+
+# working with numbers
+
+print("calculator program")
+first_num = input("first number is ")
+second_num = input("second number is ")
+print(int(first_num) + int(second_num))
