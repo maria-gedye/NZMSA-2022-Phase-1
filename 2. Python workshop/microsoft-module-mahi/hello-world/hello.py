d105b7eccba47f08fb440d95a89f4e1635d2a132
maria <mariagedye@gmail.com>
2022-05-03 21:47:07 +1200

    show today's date

--- 2. Python workshop/microsoft-module-mahi/hello-world/hello.py
+++ 2. Python workshop/microsoft-module-mahi/hello-world/hello.py
@@ -1,1 +1,11 @@
-print('Hello worlds!')
+# import module
+from datetime import date
+
+date.today()    # Call a function
+
+print(date.today())
+
+# Data type conversion
+
+print("today is the " + str(date.today()))
+
